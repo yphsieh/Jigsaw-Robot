43c0a17b4c020187601b1d31ca243b5389729976
LilyLiu0719 <lily.ylliu@gmail.com>
2020-11-30 15:50:56 +0800

    add basic function

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -1,11 +1,23 @@
 import cv2
 import numpy as np
+from argparse import ArgumentParser
 
 from detect_pieces import detect_pieces
 
+parser = ArgumentParser()
+parser.add_argument("--input_img", "-i", required=True, help="input image path")
+args = parser.parse_args()
+
 class PuzzleSolver():
-    def __init__():
-        pass
+    def __init__(self, img=None):
+        self.original_img = img
+        self.pieces = []
     
-    def main():
-        pass
+    def main(self):
+        self.pieces = detect_pieces(self.original_img)
+        print(len(self.pieces))
+
+if __name__=="__main__":
+    img = cv2.imread(args.input_img)
+    puzzle_solver = PuzzleSolver(img)
+    puzzle_solver.main()
